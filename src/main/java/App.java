c86e907f9a77a4ccefe850840d0e0ee4d20d6d5f
LacThien0907 <88417855+LacThien0907@users.noreply.github.com>
2021-09-24 20:04:00 +0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,19 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class App {
-    public static void main(String[] args) {
-//
-//        String regex = "^(0|\\+84)\\d{2}[\\.\\-\\s]?\\d{3}[\\.\\-\\s]?\\d{4}$";
-//        Pattern p = Pattern.compile(regex);
-//        String input = "0343155244, 0343 155 244, 0343-155-244, 0343.155.244";
-//        Matcher m = p.matcher(input);
-//
-//        boolean matchFound = m.find();
-//        while (matchFound){
-//            String phone = input.substring(m.start(), m.end());
-//            System.out.println(phone);
-//            matchFound = m.find();
-//        }
+    public static void main(String[] args){
 
         String text = "Hello java regex 2-12-2018, hello world 12/12/2018";
         String regex = "\\d{2}[-|/]\\d{2}[-|/]\\d{4}";
@@ -27,14 +15,21 @@ public class App {
         }
 
         String text1 = "Tôi có 2 địa chỉ email đó là: huutrongpro@gmail.com và solacthien@gmail.com";
-        String regex1 = "/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$/gm";
+        String regex1 = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}";
         Pattern p1 = Pattern.compile(regex1);
-        Matcher m1 = p.matcher(text1);
+        Matcher m1 = p1.matcher(text1);
 
         System.out.println("Địa chỉ email tìm thấy trong text1: " + text1);
         while (m1.find()) {
             System.out.println(text1.substring(m1.start(), m1.end()));
         }
+
+
+    }
+
+
+}
+
 
 
 
